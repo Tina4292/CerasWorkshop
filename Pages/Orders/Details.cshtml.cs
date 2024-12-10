d8ea75bf86a05d228df253f9f74f7f25850fc84c
Tina Coppedge <tdcoppedge1@buffs.wtamu.edu>
2024-12-10 11:43:52 -0600

    added pagination, sort, and search

--- Pages/Orders/Details.cshtml.cs
+++ Pages/Orders/Details.cshtml.cs
@@ -6,19 +6,32 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using CerasWorkshop.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
 
 namespace CerasWorkshop.Pages_Orders
 {
     public class DetailsModel : PageModel
     {
         private readonly CerasWorkshop.Models.AppDbContext _context;
+        private readonly ILogger<DetailsModel> _logger;
 
-        public DetailsModel(CerasWorkshop.Models.AppDbContext context)
+        public DetailsModel(ILogger<DetailsModel> logger, CerasWorkshop.Models.AppDbContext context)
         {
             _context = context;
+            _logger = logger;
         }
 
         public Order Order { get; set; } = default!;
+
+        [BindProperty]
+        [Display(Name = "Add Product")]
+        [Required(ErrorMessage = "Invalid Product")]
+        public int ProductIDToAdd {get; set;}
+        public SelectList ProductDropDown {get; set;} = default!;
+
+        [BindProperty]
+        public int ProductIDToDelete {get; set;}
 
         public async Task<IActionResult> OnGetAsync(int? id)
         {
@@ -27,16 +40,98 @@ namespace CerasWorkshop.Pages_Orders
                 return NotFound();
             }
 
-            var order = await _context.Orders.FirstOrDefaultAsync(m => m.OrderID == id);
+            var order = await _context.Orders.Include(po => po.ProductOrders!).ThenInclude(p => p.Product).FirstOrDefaultAsync(m => m.OrderID == id);
 
-            if (order is not null)
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else
             {
                 Order = order;
+            }
 
+            ProductDropDown = new SelectList(_context.Products.ToList(), "ProductID", "Name");
+            return Page();
+        }
+
+        public IActionResult OnPostAddProduct(int? id)
+        {
+            _logger.LogWarning($"Add Product: ProductID {id}, ADD product {ProductIDToAdd}");
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = _context.Orders.Include(po => po.ProductOrders!).ThenInclude(p => p.Product).FirstOrDefault(m => m.OrderID == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Order = order;
+            }
+
+            ProductDropDown = new SelectList(_context.Products.ToList(), "ProductID", "Name");
+
+            if (!ModelState.IsValid)
+            {
+                _logger.LogWarning($"Model State is INVALID");
                 return Page();
             }
 
-            return NotFound();
+            if (!_context.ProductOrders.Any(po => po.ProductID == ProductIDToAdd && po.OrderID == id))
+            {
+                ProductOrder productToAdd = new ProductOrder {OrderID = id.Value, ProductID = ProductIDToAdd};
+                _context.Add(productToAdd);
+                _context.SaveChanges();
+            }
+            else
+            {
+                _logger.LogWarning("Product already in order");
+            }
+
+            return Page();
+        }
+
+        public IActionResult OnPostDeleteProduct(int? id)
+        {
+            _logger.LogWarning($"Delete Product: OrderId {id}, DELETE product {ProductIDToDelete}");
+
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var order = _context.Orders.Include(po => po.ProductOrders!).ThenInclude(p => p.Product).FirstOrDefault(m => m.OrderID == id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Order = order;
+            }
+
+            ProductDropDown = new SelectList(_context.Products.ToList(), "ProductID", "Name");
+
+            var productToDelete = _context.ProductOrders.Find(ProductIDToDelete, id);
+
+            if (productToDelete != null)
+            {
+                _context.Remove(productToDelete);
+                _context.SaveChanges();
+            }
+            else
+            {
+                _logger.LogWarning("Product NOT found in order");
+            }
+
+            return RedirectToPage(new {id = id});
         }
     }
 }
--- Pages/Orders/Index.cshtml.cs
+++ Pages/Orders/Index.cshtml.cs
@@ -20,9 +20,45 @@ namespace CerasWorkshop.Pages_Orders
 
         public IList<Order> Order { get;set; } = default!;
 
+        [BindProperty(SupportsGet = true)]
+        public int PageNum {get; set;} = 1;
+        public int PageSize {get; set;} = 10;
+        public int TotalPages {get; set;}
+
+        [BindProperty(SupportsGet = true)]
+        public string CurrentSort {get; set;} = string.Empty;
+
+        [BindProperty(SupportsGet = true)]
+        public string CurrentSearch{get; set;} = string.Empty;
+
         public async Task OnGetAsync()
         {
-            Order = await _context.Orders.Include(po => po.ProductOrders!).ThenInclude(p => p.Product).ToListAsync();
+            var query = _context.Orders.Include(po => po.ProductOrders!).ThenInclude(p => p.Product).Select(p => p);
+
+            if (!string.IsNullOrEmpty(CurrentSearch))
+            {
+                query = query.Where(p => p.FirstName.ToUpper().Contains(CurrentSearch.ToUpper()) || p.LastName.ToUpper().Contains(CurrentSearch.ToUpper()) || p.ProductOrders!.Any(p => p.Product.Name.ToUpper().Contains(CurrentSearch.ToUpper())));
+            }
+
+            switch (CurrentSort)
+            {
+                case "first_asc":
+                    query = query.OrderBy(p => p.FirstName);
+                    break;
+                case "first_desc":
+                    query = query.OrderByDescending(p => p.FirstName);
+                    break;
+                case "last_asc":
+                    query = query.OrderBy(p => p.LastName);
+                    break;
+                case "last_desc":
+                    query = query.OrderByDescending(p => p.LastName);
+                    break;
+            }
+
+            TotalPages = (int)Math.Ceiling(_context.Orders.Count() / (double)PageSize);
+
+            Order = await query.Skip((PageNum-1)*PageSize).Take(PageSize).ToListAsync();
         }
     }
 }
